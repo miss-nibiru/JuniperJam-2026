4b6c0cbf52252870848a61da380d0b65fa6a5171
ValleyPenguin <valleypenguin@gmail.com>
2026-06-19 20:29:20 -0600

    Added automatic firing for base gun class
    Now the player can hold shoot, and it will automatically fire the bullets wherever they are aiming, with the appropriate delay in between each shot.

--- Assets/_Project/02_Scripts/GunBase.cs
+++ Assets/_Project/02_Scripts/GunBase.cs
@@ -1,9 +1,40 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using System.Collections;
 
 public abstract class GunBase : MonoBehaviour
 {
     [SerializeField] protected int numberOfBulletsPerShot;
     [SerializeField] protected float shootDelay;
+    [SerializeField] protected GameObject bulletPrefab;
+
+    protected bool isHoldingShoot;
+
+    protected float timeThatShootDelayWillBeOver;
 
     public abstract void Shoot(GameObject bulletPrefab);
+
+    public void OnShoot(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            isHoldingShoot = true;
+            StartCoroutine(KeepShooting());
+        }
+
+        if (context.canceled)
+        {
+            isHoldingShoot = false;
+        }
+    }
+
+    public IEnumerator KeepShooting()
+    {
+        while (isHoldingShoot && Time.time > timeThatShootDelayWillBeOver)
+        {
+            Shoot(bulletPrefab);
+            timeThatShootDelayWillBeOver = Time.time + shootDelay;
+            yield return new WaitForSeconds(shootDelay);
+        }
+    }
 }
--- Assets/_Project/02_Scripts/Shotgun.cs
+++ Assets/_Project/02_Scripts/Shotgun.cs
@@ -6,15 +6,9 @@ public class Shotgun : GunBase
 {
     [SerializeField] private float positionSpacingBetweenEachBullet;
 
-    [SerializeField] private GameObject bullet;
-
-    [SerializeField] private float autoShootDelay;
-
     [SerializeField] private float coneAngle;
 
     private Vector2 aimDirection;
-
-    private Vector2 centerOfScreen;
 
     private float angleSpacingBetweenEachBullet;
 
@@ -24,13 +18,12 @@ public class Shotgun : GunBase
 
     private float aimAngle;
 
-
     public override void Shoot(GameObject bulletPrefab)
     {
-        for (int i = 0; i <  numberOfBulletsPerShot; i++)
+        for (int i = 0; i < numberOfBulletsPerShot; i++)
         {
-            Instantiate(bulletPrefab, 
-                transform.position + new Vector3(firstBulletDistance + (i * positionSpacingBetweenEachBullet), 0f, 0f), 
+            Instantiate(bulletPrefab,
+                transform.position + new Vector3(firstBulletDistance + (i * positionSpacingBetweenEachBullet), 0f, 0f),
                 Quaternion.Euler(0f, 0f, firstBulletAngle + i * angleSpacingBetweenEachBullet + aimAngle));
         }
     }
@@ -40,21 +33,6 @@ public class Shotgun : GunBase
         firstBulletAngle = coneAngle / -2f;
         angleSpacingBetweenEachBullet = coneAngle / (numberOfBulletsPerShot - 1);
         firstBulletDistance = -positionSpacingBetweenEachBullet * (numberOfBulletsPerShot - 1) / 2f;
-    }
-
-    private void Start()
-    {
-        StartCoroutine(ShootBulletsOverTime());
-        centerOfScreen = new Vector2(Screen.width * .5f, Screen.height * .5f);
-    }
-
-    private IEnumerator ShootBulletsOverTime()
-    {
-        while (true)
-        {
-            Shoot(bullet);
-            yield return new WaitForSeconds(autoShootDelay);
-        }
     }
 
     private void Update()
