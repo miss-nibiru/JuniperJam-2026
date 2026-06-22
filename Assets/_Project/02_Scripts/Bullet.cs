fb66e59ddf42090b2d3a4cf7b11aebac58609bc9
miss-nibiru <du.herbst@gmail.com>
2026-06-21 19:27:20 -0600

    Projectiles
    # Conflicts:
    #	Assets/_Project/01_Scenes/AllanScene.unity
    #	Assets/_Project/02_Scripts/Bullet.cs

--- Assets/_Project/02_Scripts/Bullet.cs
+++ Assets/_Project/02_Scripts/Bullet.cs
@@ -5,19 +5,28 @@ public class Bullet : MonoBehaviour
 {
     [SerializeField] private float bulletSpeed;
     [SerializeField] private float lifetime;
+    [SerializeField] private int bulletDamage;
 
     private Rigidbody2D rb;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        Destroy(gameObject, lifetime);
     }
 
     private void Update()
     {
         rb.freezeRotation = true;
         rb.linearVelocity = transform.up * bulletSpeed;
+    }
 
-        Destroy(gameObject, lifetime);
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        EnemyController enemy = collision.GetComponent<EnemyController>();
+        if (!enemy) return;
+        enemy.TakeDamage(bulletDamage);
+        Destroy(gameObject);
+
     }
 }
