5b7802936eb8e7a52b54f5e93a541a24953afedc
ValleyPenguin <valleypenguin@gmail.com>
2026-06-21 19:59:47 -0600

    Revert "Projectiles"
    This reverts commit fb66e59ddf42090b2d3a4cf7b11aebac58609bc9.

--- Assets/_Project/02_Scripts/Bullet.cs
+++ Assets/_Project/02_Scripts/Bullet.cs
@@ -5,28 +5,19 @@ public class Bullet : MonoBehaviour
 {
     [SerializeField] private float bulletSpeed;
     [SerializeField] private float lifetime;
-    [SerializeField] private int bulletDamage;
 
     private Rigidbody2D rb;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        Destroy(gameObject, lifetime);
     }
 
     private void Update()
     {
         rb.freezeRotation = true;
         rb.linearVelocity = transform.up * bulletSpeed;
-    }
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        EnemyController enemy = collision.GetComponent<EnemyController>();
-        if (!enemy) return;
-        enemy.TakeDamage(bulletDamage);
-        Destroy(gameObject);
-
+        Destroy(gameObject, lifetime);
     }
 }
