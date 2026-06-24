640b371e3affec33d3300acbe0d46dc05cb6d7b3
ValleyPenguin <valleypenguin@gmail.com>
2026-06-24 09:14:00 -0600

    Added Working Buttons to Panels and Main Menu

---
+++ Assets/_Project/02_Scripts/GameButtonActions.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameButtonActions : MonoBehaviour
+{
+    [SerializeField] private string gameSceneName = "AllanScene";
+    [SerializeField] private GameObject creditsPanel;
+    [SerializeField] private GameObject winPanel;
+
+    public void StartGame()
+    {
+        LoadScene(gameSceneName);
+    }
+
+    public void PlayAgain()
+    {
+        LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+
+    public void ShowCredits()
+    {
+        if (winPanel) winPanel.SetActive(false);
+        if (creditsPanel) creditsPanel.SetActive(true);
+    }
+
+    private void LoadScene(string sceneName)
+    {
+        if (string.IsNullOrWhiteSpace(sceneName)) return;
+
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(sceneName);
+    }
+}
