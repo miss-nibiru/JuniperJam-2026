af08e60ebe4e88c16a4e23577a9005533bc24bf7
ValleyPenguin <valleypenguin@gmail.com>
2026-06-25 13:45:56 -0600

    Added Player Spin Animation for Dash

--- Assets/_Project/02_Scripts/_PlayerScripts/MoveAbilityDash.cs
+++ Assets/_Project/02_Scripts/_PlayerScripts/MoveAbilityDash.cs
@@ -6,15 +6,58 @@ public class MoveAbilityDash : MoveAbility
 {
     [SerializeField] private float dashDuration;
     [SerializeField] private float dashForce;
+    [SerializeField] private string dashAnimationTrigger;
 
     private Rigidbody2D rb;
+    private Animator playerAnimator;
+    private SpriteRenderer playerSpriteRenderer;
+    private Sprite restingSprite;
+
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        playerAnimator = GetComponentInChildren<Animator>();
+
+        if (playerAnimator)
+        {
+            playerSpriteRenderer = playerAnimator.GetComponent<SpriteRenderer>();
+            if (playerSpriteRenderer) restingSprite = playerSpriteRenderer.sprite;
+
+            playerAnimator.enabled = false;
+        }
     }
+
     public override IEnumerator UseMoveAbility(PlayerMove playerMove, Vector2 direction)
     {
+        PlayDashAnimation();
         rb.AddForce(direction * dashForce);
         yield return new WaitForSeconds(dashDuration);
+        StopDashAnimation();
+    }
+
+    private void PlayDashAnimation()
+    {
+        if (!playerAnimator) return;
+        if (string.IsNullOrEmpty(dashAnimationTrigger)) return;
+
+        playerAnimator.enabled = true;
+        playerAnimator.SetTrigger(dashAnimationTrigger);
+    }
+
+    private void StopDashAnimation()
+    {
+        if (!playerAnimator) return;
+
+        if (!string.IsNullOrEmpty(dashAnimationTrigger))
+        {
+            playerAnimator.ResetTrigger(dashAnimationTrigger);
+        }
+
+        playerAnimator.enabled = false;
+
+        if (playerSpriteRenderer && restingSprite)
+        {
+            playerSpriteRenderer.sprite = restingSprite;
+        }
     }
 }
