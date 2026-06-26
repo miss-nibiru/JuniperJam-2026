0186ddbe99b0a7d40a99ab12ae80b013cceeab22
ValleyPenguin <valleypenguin@gmail.com>
2026-06-26 00:19:16 -0600

    Added Camera Shake on Player Damage

--- Assets/_Project/02_Scripts/_PlayerScripts/PlayerHealth.cs
+++ Assets/_Project/02_Scripts/_PlayerScripts/PlayerHealth.cs
@@ -1,10 +1,16 @@
 using System;
+using UnityEditor.Rendering.LookDev;
 using UnityEngine;
+using System.Collections;
 
 public class PlayerHealth : MonoBehaviour, IHealthBars
 {
     [SerializeField] private int maxHealth = 5;
     [SerializeField] private RunStateManager runStateManager;
+
+    [SerializeField] private Transform cameraShake;
+    [SerializeField] private float shakeDuration;
+    [SerializeField] private float cameraShakeStrength;
 
     private int _currentHealth;
     private bool _isDead;
@@ -31,6 +37,7 @@ public class PlayerHealth : MonoBehaviour, IHealthBars
     {
         if(_isDead) return;
 
+        StartCoroutine(ShakeCamera());
         _currentHealth -= damageAmount;
         _currentHealth = Mathf.Clamp(_currentHealth, 0, maxHealth);
         HealthChanged?.Invoke(_currentHealth, maxHealth);
@@ -67,5 +74,24 @@ public class PlayerHealth : MonoBehaviour, IHealthBars
         }
     }
 
-    
+    private IEnumerator ShakeCamera()
+    {
+        Vector3 originalCameraPosition = cameraShake ? cameraShake.localPosition : Vector3.zero;
+        float timer = 0f;
+
+        while (timer < shakeDuration)
+        {
+            timer += Time.deltaTime;
+
+            if (cameraShake)
+            {
+                float cameraX = UnityEngine.Random.Range(-cameraShakeStrength, cameraShakeStrength);
+                float cameraY = UnityEngine.Random.Range(-cameraShakeStrength, cameraShakeStrength);
+                cameraShake.localPosition = originalCameraPosition + new Vector3(cameraX, cameraY, 0f);
+            }
+
+            yield return null;
+        }
+
+        if (cameraShake) cameraShake.localPosition = originalCameraPosition;    }
 }
